217ea1ad6df16b286e16756739d77b43d10d2719
Paul Cortes <paul.cortes@Paul-Cortes-MacBook-Pro.local>
2022-05-25 22:11:45 -0500

    Corazon creciente

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 const accelerate = (counter) => {
     if (counter % 20 == 0 && delay > 10) {
         delay -= 5;
-        console.log(`delay -5: ${delay}`)
+        // console.log(`delay -5: ${delay}`)
     } else if (counter % 100 == 0 && delay > 1 && delay <= 10) {
         delay -= 1;
-        console.log(`delay -1: ${delay}`)
+        // console.log(`delay -1: ${delay}`)
     }
 }
 
@@ -19,12 +19,17 @@ const showTeAmoV2 = async () => {
 
     const teAmo = document.createElement("div");
     teAmo.className = 'te-amo-line';
-    teAmo.innerHTML = `<span>Te amo</span> <span id="veces">1</span>❤️ <span> veces mi amor<span/>`;
+    teAmo.innerHTML = `<span>Te amo</span> <span id="veces">1</span><span id="heart">❤️</span> <span> veces mi amor<span/>`;
     miAmor.appendChild(teAmo)
+    let heartSize = 48;
     for (let veces = 1; veces <= infinito; veces++) {
         let contador = document.querySelector("#veces")
+        let heart = document.querySelector("#heart") 
         contador.className = "veces"
         contador.innerHTML = ` ${veces} `;
+        heart.style.fontSize = heartSize+"px"
+        heartSize += 0.1;
+
         await waiting();
         accelerate(veces);
     }
